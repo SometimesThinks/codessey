0441e24b2a04d45f2abe9982b852caef79ab1d66
SometimesThinks <ycw2859@gmail.com>
2026-04-04 14:33:57 +0900

    feat: Quiz 클래스 정의 및 퀴즈 출제/정답 확인 메소드 구현

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+
+    def display_quiz(self):
+        print(self.question)
+        for i, choice in enumerate(self.choices):
+            print(f"{i + 1}. {choice}")
+
+    def check_answer(self, answer):
+        return self.answer == answer
+
+
 class QuizGame:
     def __init__(self):
         self.quizzes = []
