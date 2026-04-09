2da6bc37b8ff5d2b34e8e9823945720c8c61e93d
SometimesThinks <ycw2859@gmail.com>
2026-04-09 11:34:19 +0900

    chore: input 파일 정리 및 연결점 변경

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from controllers import Controller
 
 
 def main():
-    sys.stdin = open("dummy/input_1.txt", "r")
+    sys.stdin = open("input.txt", "r")
 
     controller = Controller()
     controller.run()
